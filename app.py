3609736ff65b90c7a43202a3367231cac46f8678
anteater299 <anteater299@gmail.com>
2025-02-12 18:07:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -59,14 +59,86 @@ def webhook():
                                     ]
                                 },
                                 {
-                                    "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/198284/image/MAI_174423644_X700X700.jpg",
-                                    "title": "(水果)日本青森葉乃果蘋果中果32規300g*6粒禮盒*2盒",
-                                    "text": "$2888",
+                                    "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/166928/image/MAI_154734938_X700X700.jpg",
+                                    "title": "【韓國innisfree】無油無慮礦物控油蜜粉5g [買1送1]",
+                                    "text": "$458",
                                     "actions": [
                                         {
                                             "type": "uri",
                                             "label": "查看商品",
-                                            "uri": "https://openlife.7-11.com.tw/comm/share/share.html?v=e88a81727c47914204f9"
+                                            "uri": "https://openlife.7-11.com.tw/comm/share/share.html?v=28fee610fa12a55b9ad6"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/202232/image/MAI_192711319_X700X700.jpg",
+                                    "title": "米樂爆米花- 史努比花季寫生罐爆米花(10入/箱)-焦糖",
+                                    "text": "$1780",
+                                    "actions": [
+                                        {
+                                            "type": "uri",
+                                            "label": "查看商品",
+                                            "uri": "https://openlife.7-11.com.tw/comm/share/share.html?v=ad5cda379f38a0d8b866"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/202226/image/MAI_183454795_X400X400.jpg?uts=175802593",
+                                    "title": "【超比食品】燕麥脆片-微甜草莓x6包(100g/包)",
+                                    "text": "$699",
+                                    "actions": [
+                                        {
+                                            "type": "uri",
+                                            "label": "查看商品",
+                                            "uri": "https://openlife.7-11.com.tw/comm/share/share.html?v=37d2a05d12e46913a85e"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/201025/image/MAI_111508012_X400X400.jpg?uts=175802593",
+                                    "title": "時髦的安吉小姐 可微波陶瓷碗(黃/白) 含蓋",
+                                    "text": "$249",
+                                    "actions": [
+                                        {
+                                            "type": "uri",
+                                            "label": "查看商品",
+                                            "uri": "https://openlife.7-11.com.tw/comm/share/share.html?v=634f8d8e1425dc761bb6"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/199996/image/MAI_152944914_X400X400.jpg?uts=175802593",
+                                    "title": "寶可夢手提燜燒罐(三色選)",
+                                    "text": "$593",
+                                    "actions": [
+                                        {
+                                            "type": "uri",
+                                            "label": "查看商品",
+                                            "uri": "https://openlife.7-11.com.tw/comm/share/share.html?v=85141b1306c58b63288d"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/195847/image/MAI_153641158_X400X400.jpg?uts=175802593",
+                                    "title": "【利捷維】有酵超級B群膜衣錠 60錠",
+                                    "text": "$429",
+                                    "actions": [
+                                        {
+                                            "type": "uri",
+                                            "label": "查看商品",
+                                            "uri": "https://openlife.7-11.com.tw/comm/share/share.html?v=aae26f7afe62d7df4719"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/178160/image/MAI_171909057_X400X400.png?uts=175802593",
+                                    "title": "【羅技 Logitech】Pebble 2 Combo 無線藍芽 鍵盤滑鼠組",
+                                    "text": "$1,790",
+                                    "actions": [
+                                        {
+                                            "type": "uri",
+                                            "label": "查看商品",
+                                            "uri": "https://openlife.7-11.com.tw/comm/share/share.html?v=af996c2aa30334fe9e30"
                                         }
                                     ]
                                 },
@@ -82,7 +154,7 @@ def webhook():
                                         }
                                     ]
                                 },
-                                {
+                                 {
                                     "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/198284/image/MAI_174423644_X700X700.jpg",
                                     "title": "(水果)日本青森葉乃果蘋果中果32規300g*6粒禮盒*2盒",
                                     "text": "$2888",
@@ -94,55 +166,7 @@ def webhook():
                                         }
                                     ]
                                 },
-                                {
-                                    "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/198284/image/MAI_174423644_X700X700.jpg",
-                                    "title": "(水果)日本青森葉乃果蘋果中果32規300g*6粒禮盒*2盒",
-                                    "text": "$2888",
-                                    "actions": [
-                                        {
-                                            "type": "uri",
-                                            "label": "查看商品",
-                                            "uri": "https://openlife.7-11.com.tw/comm/share/share.html?v=e88a81727c47914204f9"
-                                        }
-                                    ]
-                                },
-                                {
-                                    "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/198284/image/MAI_174423644_X700X700.jpg",
-                                    "title": "(水果)日本青森葉乃果蘋果中果32規300g*6粒禮盒*2盒",
-                                    "text": "$2888",
-                                    "actions": [
-                                        {
-                                            "type": "uri",
-                                            "label": "查看商品",
-                                            "uri": "https://openlife.7-11.com.tw/comm/share/share.html?v=e88a81727c47914204f9"
-                                        }
-                                    ]
-                                },
-                                {
-                                    "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/198284/image/MAI_174423644_X700X700.jpg",
-                                    "title": "(水果)日本青森葉乃果蘋果中果32規300g*6粒禮盒*2盒",
-                                    "text": "$2888",
-                                    "actions": [
-                                        {
-                                            "type": "uri",
-                                            "label": "查看商品",
-                                            "uri": "https://openlife.7-11.com.tw/comm/share/share.html?v=e88a81727c47914204f9"
-                                        }
-                                    ]
-                                },
-                                {
-                                    "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/198284/image/MAI_174423644_X700X700.jpg",
-                                    "title": "(水果)日本青森葉乃果蘋果中果32規300g*6粒禮盒*2盒",
-                                    "text": "$2888",
-                                    "actions": [
-                                        {
-                                            "type": "uri",
-                                            "label": "查看商品",
-                                            "uri": "https://openlife.7-11.com.tw/comm/share/share.html?v=e88a81727c47914204f9"
-                                        }
-                                    ]
-                                },
-                                {
+                                 {
                                     "thumbnailImageUrl": "https://711go.7-11.com.tw/Files/market/198284/image/MAI_174423644_X700X700.jpg",
                                     "title": "(水果)日本青森葉乃果蘋果中果32規300g*6粒禮盒*2盒",
                                     "text": "$2888",
